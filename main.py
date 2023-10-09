c3f81f5f3a2d82ef34b13f234df016dd5b9c5e71
ChBLA <cbla19@student.aau.dk>
2023-10-09 14:58:20 +0200

    fixed submodule

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from qiskit import QuantumCircuit
 from quimb.tensor import Circuit
 import cotengra as ctg
 import numpy as np
-from tddpure.TDD.TDD import Ini_TDD, TDD
-from tddpure.TDD.TN import Index,Tensor,TensorNetwork
-from tddpure.TDD.TDD_Q import cir_2_tn,get_real_qubit_num,add_trace_line,add_inputs,add_outputs
+from tddfork.TDD.TDD import Ini_TDD, TDD
+from tddfork.TDD.TN import Index,Tensor,TensorNetwork
+from tddfork.TDD.TDD_Q import cir_2_tn,get_real_qubit_num,add_trace_line,add_inputs,add_outputs
 
 def makeCircuit():
     circ = QuantumCircuit(3)
