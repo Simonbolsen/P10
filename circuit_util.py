e07c0d636d28656c67be3b08d979d9943662deb4
ChBLA <cbla19@student.aau.dk>
2023-10-11 11:57:35 +0200

    added circuit functions

--- circuit_util.py
+++ circuit_util.py
@@ -1,6 +1,8 @@
 from mqt.ddsim.pathqasmsimulator import create_tensor_network
 from qiskit import QuantumCircuit
 from quimb.tensor import Circuit
+from quimb.tensor.circuit import Gate
+from string import Template
 import cotengra as ctg
 import numpy as np
 import random
@@ -34,10 +36,43 @@ def get_example_circuit(n):
 
     return circ
 
+def get_qiskit_example_circuit() -> QuantumCircuit:
+    circ = QuantumCircuit(3)
+    circ.h(0)
+    circ.cx(0, 1)
+    circ.cx(1, 2)
+    return circ
+
 def quimb_to_qiskit_circuit(quimb_circuit: Circuit):
-    circ_qasm = quimb_circuit.qasm()
+    circ_qasm = quimb_circuit
     return QuantumCircuit.from_qasm_str(circ_qasm)
 
 def qiskit_to_quimb_circuit(qiskit_circuit: QuantumCircuit):
     circ_qasm = qiskit_circuit.qasm()
     return Circuit.from_openqasm2_str(circ_qasm)
+
+
+def _quimb_to_qasm(circuit: Circuit):
+    temp = Template("")
+    
+    properties = {
+
+    }
+
+    return temp.substitute(properties)
+
+
+def get_gate_template(gate: Gate) -> Template:
+    temp = Template('$name$params $qubit')
+    
+    properties = {
+        'name' : gate.label,
+        'params' : '' if len(gate.params) == 0 else str(gate.params),
+        'qubit' : get_qubit_template(gate.qubits)
+    }
+    return temp.substitute(properties)
+
+def get_qubit_template(qubits):
+    if len(qubits) == 1:
+        return Template('q[$index]').substitute({'index' : qubits[0]})
+    return Template('q[$i1], q[$i2]').substitute({'i1' : qubits[0], 'i2' : qubits[1]})
