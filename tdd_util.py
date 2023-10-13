ca84ae20b3f8a7ac5e6a73197414e890b08025cd
Simon <Simonbolsen1234@gmail.com>
2023-10-13 15:00:10 +0200

    equivalence checking in main

--- tdd_util.py
+++ tdd_util.py
@@ -1,13 +1,8 @@
 from mqt.ddsim.pathqasmsimulator import create_tensor_network
 from qiskit import QuantumCircuit
-from quimb.tensor import Circuit
-import cotengra as ctg
-import numpy as np
-import random
 from tddfork.TDD.TDD import Ini_TDD, TDD, cont
 from tddfork.TDD.TN import Index,Tensor,TensorNetwork
 from tddfork.TDD.TDD_Q import cir_2_tn,get_real_qubit_num,add_trace_line,add_inputs,add_outputs
-import circuit_util as cu
 
 def tn_to_tdd(tn: TensorNetwork):
     return tn.cont()
